0106a88aa85087fd9da61eb4ff0fee33f9cc003b
Rafael Morales <116736888+Rafaelmorales1982@users.noreply.github.com>
2025-03-18 23:49:35 +0000

    teste

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+   
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
